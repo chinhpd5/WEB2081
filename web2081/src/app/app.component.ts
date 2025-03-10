21c708f0eee388eb8e262f1fc0817b8343c7f824
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-10 18:13:13 +0700

    tailwind

--- web2081/src/app/app.component.ts
+++ web2081/src/app/app.component.ts
@@ -11,9 +11,14 @@ export class AppComponent {
   data = {
     name: "chinhpd5",
     age: 20,
-    gender: true,
+    gender: false,
     avatar: "https://upload.wikimedia.org/wikipedia/commons/d/d7/Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg",
     description: "Yêu màu tím",
     linkFB: "https://google.com"
+  }
+
+  // phương thức
+  renderGender(): string {
+    return this.data.gender ? "Nam": "Nữ"
   }
 }
