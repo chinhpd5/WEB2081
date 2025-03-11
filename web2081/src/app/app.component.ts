31a183c021c26d4128563a54528f00b991e90977
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-11 17:41:55 +0700

    binding

---
+++ web2081/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'web2081';
+  data = {
+    name: "chinhpd5",
+    age: 20,
+    gender: true,
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/d/d7/Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg",
+    description: "Yêu màu tím",
+    linkFB: "https://google.com"
+  }
+}
