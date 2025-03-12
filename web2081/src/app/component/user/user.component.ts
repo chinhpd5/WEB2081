494c14fa1cccb8be22180221a949a19376f7dd6f
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-12 17:13:17 +0700

    users

---
+++ web2081/src/app/component/user/user.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  users = [
+    {
+      id: 1,
+      name: 'Nguyễn Văn A',
+      email: 'nguyenvana@example.com',
+      age: 25,
+      gender: true,
+      isActive: true,
+      role: 'admin',
+    },
+    {
+      id: 2,
+      name: 'Trần Thị B',
+      email: 'tranthib@example.com',
+      age: 30,
+      gender: false,
+      isActive: false,
+      role: 'admin',
+    },
+    {
+      id: 3,
+      name: 'Lê Văn C',
+      email: 'levanc@example.com',
+      age: 22,
+      gender: true,
+      isActive: true,
+      role: 'member',
+    },
+  ];
+}
