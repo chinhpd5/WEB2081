9d42d1bdebcdf72c24bd30fdc053e8c060855722
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-13 19:44:58 +0700

    ngFor

--- web2081/src/app/component/user/user.component.ts
+++ web2081/src/app/component/user/user.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
