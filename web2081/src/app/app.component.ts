44fb824c868b9b74c3e44c3338b600b4e3e124d8
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-13 18:59:12 +0700

    tạo component thủ công

--- web2081/src/app/app.component.ts
+++ web2081/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
 
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent]
 })
 
 export class AppComponent {
