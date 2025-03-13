7481739855c98ba7b33f23140b1406aa3be24880
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-13 19:13:50 +0700

    user

--- web2081/src/app/app.component.ts
+++ web2081/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './component/header/header.component';
+import { UserComponent } from "./component/user/user.component";
 
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, UserComponent]
 })
 
 export class AppComponent {
