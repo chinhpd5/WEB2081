bcdfc17d1c9d1e8e0a5d60b64b434b6396521a40
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-14 16:58:04 +0700

    add job

--- web2081/src/app/component/todolist/todolist.component.ts
+++ web2081/src/app/component/todolist/todolist.component.ts
@@ -1,16 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
 @Component({
   selector: 'app-todolist',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './todolist.component.html',
   styleUrl: './todolist.component.css'
 })
 export class TodolistComponent {
+  job= ''
   jobs = [
     "Ăn",
     "Ngủ",
     "Code"
   ]
+  handleAddJob (){
+    // console.log(this.job);
+    this.jobs.push(this.job)
+    this.job=''
+    
+  }
 }
