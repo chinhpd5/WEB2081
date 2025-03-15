d26fc04d02e8fea629fcb9ffed74b464f3ae7bd4
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-15 19:20:17 +0700

    Input

--- web2081/src/app/app.component.ts
+++ web2081/src/app/app.component.ts
@@ -3,16 +3,61 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './component/header/header.component';
 import { UserComponent } from "./component/user/user.component";
 import { TodolistComponent } from "./component/todolist/todolist.component";
+import { ProductComponent } from "./component/product/product.component";
 
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [TodolistComponent]
+  imports: [TodolistComponent, ProductComponent]
 })
 
 export class AppComponent {
+  myName = 'chinhpd5'
+  myAge = 25
+  products = [
+    {
+      id: 1,
+      name: 'Laptop Dell XPS 15',
+      price: 35000000,
+      inStock: true,
+      category: 'LT',
+      rating: 4.5,
+    },
+    {
+      id: 2,
+      name: 'iPhone 14 Pro Max',
+      price: 29000000,
+      inStock: false,
+      category: 'MB',
+      rating: 4.8,
+    },
+    {
+      id: 3,
+      name: 'Samsung Galaxy S23',
+      price: 22000000,
+      inStock: true,
+      category: 'MB',
+      rating: 4.6,
+    },
+    {
+      id: 4,
+      name: 'Tai nghe Sony WH-1000XM4',
+      inStock: true,
+      category: 'PK',
+      rating: 4.7,
+    },
+    {
+      id: 5,
+      name: 'Bàn phím cơ Keychron K8',
+      price: 2500000,
+      inStock: false,
+      category: 'PK',
+      rating: 4.3,
+    },
+  ];
+
   // title = 'web2081';
   data = {
     name: "chinhpd5",
--- web2081/src/app/component/product/product.component.ts
+++ web2081/src/app/component/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -7,47 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  products = [
-    {
-      id: 1,
-      name: 'Laptop Dell XPS 15',
-      price: 35000000,
-      inStock: true,
-      category: 'LT',
-      rating: 4.5,
-    },
-    {
-      id: 2,
-      name: 'iPhone 14 Pro Max',
-      price: 29000000,
-      inStock: false,
-      category: 'MB',
-      rating: 4.8,
-    },
-    {
-      id: 3,
-      name: 'Samsung Galaxy S23',
-      price: 22000000,
-      inStock: true,
-      category: 'MB',
-      rating: 4.6,
-    },
-    {
-      id: 4,
-      name: 'Tai nghe Sony WH-1000XM4',
-      inStock: true,
-      category: 'PK',
-      rating: 4.7,
-    },
-    {
-      id: 5,
-      name: 'Bàn phím cơ Keychron K8',
-      price: 2500000,
-      inStock: false,
-      category: 'PK',
-      rating: 4.3,
-    },
-  ];
+  @Input() name!: string;
+  @Input() age!: number
+
+  
 
   /**
    * Tạo component 'product'
