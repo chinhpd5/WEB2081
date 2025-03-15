3ca0eadbb1300da7140742229a2a40e9ce656dd3
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-15 18:40:33 +0700

    list jobs

--- web2081/src/app/app.component.ts
+++ web2081/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './component/header/header.component';
 import { UserComponent } from "./component/user/user.component";
+import { TodolistComponent } from "./component/todolist/todolist.component";
 
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent]
+  imports: [TodolistComponent]
 })
 
 export class AppComponent {
---
+++ web2081/src/app/component/todolist/todolist.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todolist',
+  imports: [],
+  templateUrl: './todolist.component.html',
+  styleUrl: './todolist.component.css'
+})
+export class TodolistComponent {
+
+  jobs=[
+    "Ăn",
+    "Ngủ",
+    "Code"
+  ]
+}
