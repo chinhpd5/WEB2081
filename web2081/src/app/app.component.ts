cd29766c4f765f32647815909967b6c9ed4aabd8
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-15 17:26:16 +0700

    Input

--- web2081/src/app/app.component.ts
+++ web2081/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { TodolistComponent } from "./component/todolist/todolist.component";
 
 @Component({
   selector: 'app-root',
-  imports: [TodolistComponent],
+  imports: [TodolistComponent, ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -21,4 +21,49 @@ export class AppComponent {
     description: "Yêu màu tím",
     linkFB: "https://google.com"
   }
+
+  myName ="chinhpd5"
+  myAge = 30
+
+  products = [
+    {
+      id: 1,
+      name: 'Laptop Dell XPS 15',
+      price: 35000000,
+      inStock: true,
+      category: 'LT',
+      rating: 4.5,
+    },
+    {
+      id: 2,
+      name: 'iPhone 14 Pro Max',
+      price: 29000000,
+      inStock: false,
+      category: 'MB',
+      rating: 4.8,
+    },
+    {
+      id: 3,
+      name: 'Samsung Galaxy S23',
+      price: 22000000,
+      inStock: true,
+      category: 'MB',
+      rating: 4.6,
+    },
+    {
+      id: 4,
+      name: 'Tai nghe Sony WH-1000XM4',
+      inStock: true,
+      category: 'PK',
+      rating: 4.7,
+    },
+    {
+      id: 5,
+      name: 'Bàn phím cơ Keychron K8',
+      price: 2500000,
+      inStock: false,
+      category: 'PK',
+      rating: 4.3,
+    },
+  ];
 }
