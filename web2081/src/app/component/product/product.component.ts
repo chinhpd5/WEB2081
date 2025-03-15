5e10c5bb2caf12f7dd066312b707ad56924dbd1a
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-15 19:47:06 +0700

    product

--- web2081/src/app/component/product/product.component.ts
+++ web2081/src/app/component/product/product.component.ts
@@ -1,16 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
   @Input() name!: string;
   @Input() age!: number
+  @Input() products!: any;
 
-  
+  renderSatus(status: boolean){
+    return status ? 'Còn hàng': 'Hết hàng'
+  }
 
   /**
    * Tạo component 'product'
