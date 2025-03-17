923f4fb4e0081435f60297483a7562ed33e94ec9
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-17 17:06:22 +0700

    update

--- web2081/src/app/component/lifecycle/lifecycle.component.ts
+++ web2081/src/app/component/lifecycle/lifecycle.component.ts
@@ -29,7 +29,7 @@ export class LifecycleComponent {
   ngAfterViewInit() {
     console.log('ngAfterViewInit called');
   }
-  ngAfterContentChecked() {
+  ngAfterViewChecked() {
     console.log('ngAfterViewChecked called');
   }
   ngOnDestroy() {
