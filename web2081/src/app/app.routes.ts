d515edfbb76cbccc84e5efa0f9d22284dddb41d2
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-17 17:47:53 +0700

    router

--- web2081/src/app/app.routes.ts
+++ web2081/src/app/app.routes.ts
@@ -2,15 +2,25 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { ClientComponent } from './layout/client/client.component';
 
 export const routes: Routes = [
   {
-    path: '', component: HomeComponent
+    path: '', component: ClientComponent,
+    children: [
+      {
+        path: '', component: HomeComponent
+      },
+      {
+        path: 'product', component: ProductsComponent
+      },
+      {
+        path: 'about', component: AboutComponent
+      }
+    ]
   },
   {
-    path: 'product', component: ProductsComponent
-  },
-  {
-    path: 'about', component: AboutComponent
+    path: "**", component: NotfoundComponent
   }
 ];
