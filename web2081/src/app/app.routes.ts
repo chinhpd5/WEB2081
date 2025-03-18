5b3c064359ece9c21d115a5514822b8ad1d5dc9e
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-18 19:53:28 +0700

    client router

--- web2081/src/app/app.routes.ts
+++ web2081/src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { HomeComponent } from './page/home/home.component';
 import { ProductsComponent } from './page/products/products.component';
 import { AboutComponent } from './page/about/about.component';
 import { NotfoundComponent } from './page/notfound/notfound.component';
+import { ClientComponent } from './layout/client/client.component';
 
 export const routes: Routes = [
-  {path:'', component: HomeComponent}, // trang chủ
-  {path:'product', component: ProductsComponent}, // sản phẩm
-  {path:'about', component: AboutComponent}, // about
+  {
+    path: '', component: ClientComponent, children: [
+      {path:'', component: HomeComponent}, // trang chủ
+      {path:'product', component: ProductsComponent}, // sản phẩm
+      {path:'about', component: AboutComponent}, // about
+    ]
+  },
   {path:'**', component: NotfoundComponent}
 ];
