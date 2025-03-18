bd983fd20f1bb1db92b46e92197f2a731386b0f9
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-18 19:33:59 +0700

    router

--- web2081/src/app/app.component.ts
+++ web2081/src/app/app.component.ts
@@ -1,90 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './component/header/header.component';
-import { UserComponent } from "./component/user/user.component";
-import { TodolistComponent } from "./component/todolist/todolist.component";
-import { ProductComponent } from "./component/product/product.component";
-import { LifecycleComponent } from "./component/lifecycle/lifecycle.component";
+
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [TodolistComponent, ProductComponent, LifecycleComponent]
 })
 
 export class AppComponent {
-  isVisible= true
-  handleChangeVisible(){
-    this.isVisible = !this.isVisible
-  }
-
-  value='chinhpd5'
-  handleChangeInput(){
-    this.value = 'chinhpd5 new'
-  }
-
-  myName = 'chinhpd5'
-  myAge = 25
-  products = [
-    {
-      id: 1,
-      name: 'Laptop Dell XPS 15',
-      price: 35000000,
-      inStock: true,
-      category: 'LT',
-      rating: 4.5,
-    },
-    {
-      id: 2,
-      name: 'iPhone 14 Pro Max',
-      price: 29000000,
-      inStock: false,
-      category: 'MB',
-      rating: 4.8,
-    },
-    {
-      id: 3,
-      name: 'Samsung Galaxy S23',
-      price: 22000000,
-      inStock: true,
-      category: 'MB',
-      rating: 4.6,
-    },
-    {
-      id: 4,
-      name: 'Tai nghe Sony WH-1000XM4',
-      inStock: true,
-      category: 'PK',
-      rating: 4.7,
-    },
-    {
-      id: 5,
-      name: 'Bàn phím cơ Keychron K8',
-      price: 2500000,
-      inStock: false,
-      category: 'PK',
-      rating: 4.3,
-    },
-  ];
-
-  receiveId(id: number){
-    // console.log(id);
-    this.products = this.products.filter(item=> item.id != id)
-  }
-
-  // title = 'web2081';
-  data = {
-    name: "chinhpd5",
-    age: 20,
-    gender: false,
-    avatar: "https://upload.wikimedia.org/wikipedia/commons/d/d7/Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg",
-    description: "Yêu màu tím",
-    linkFB: "https://google.com"
-  }
-
-  renderGender():string{
-    return this.data.gender ? "Nam": "Nữ"
-  }
+  
 }
--- web2081/src/app/app.routes.ts
+++ web2081/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { ProductsComponent } from './page/products/products.component';
+import { AboutComponent } from './page/about/about.component';
+import { NotfoundComponent } from './page/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component: HomeComponent}, // trang chủ
+  {path:'product', component: ProductsComponent}, // sản phẩm
+  {path:'about', component: AboutComponent}, // about
+  {path:'**', component: NotfoundComponent}
+];
