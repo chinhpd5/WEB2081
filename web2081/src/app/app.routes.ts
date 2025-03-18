dd80d8afa0de798628bab0784db36a9333acb883
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-18 17:42:37 +0700

    layout

--- web2081/src/app/app.routes.ts
+++ web2081/src/app/app.routes.ts
@@ -3,19 +3,21 @@ import { HomeComponent } from './page/home/home.component';
 import { AboutComponent } from './page/about/about.component';
 import { ProductsComponent } from './page/products/products.component';
 import { NotfoundComponent } from './page/notfound/notfound.component';
+import { ClientComponent } from './layout/client/client.component';
 
 export const routes: Routes = [
   {
-    path:'',
-    component: HomeComponent
-  },
-  {
-    path:'about',
-    component: AboutComponent
-  },
-  {
-    path:'product',
-    component: ProductsComponent
+    path: '', component: ClientComponent, children:[
+      {
+        path:'', component: HomeComponent
+      },
+      {
+        path:'about', component: AboutComponent
+      },
+      {
+        path:'product', component: ProductsComponent
+      },
+    ]
   },
   {
     path:"**",
