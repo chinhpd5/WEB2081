648a47a845ee8cbd87df0463ec3746816c0fe109
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-21 16:56:01 +0700

    service

--- web2081/src/app/pages/home/home.component.ts
+++ web2081/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../service/product.service';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,33 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  //B1: tạo file db.json copy data
+  //B2: tạo script "server" trong package.json
+  //B3: Tạo service: ng g s service/product
+  //B4: add provide provideHttpClient vào app.config.ts
+  //B5: Tạo service getList
+  //B6: Gọi service getList tại home
+
+  constructor(private productService: ProductService){}
+
+  ngOnInit(){
+    // call api lấy danh sách sản phẩm
+    this.productService.getList().subscribe({
+      next: (data) => {
+        // xử lý logic thành công
+        console.log(data);
+      },
+      error: (err)=>{
+        // xử lý logic nếu thất bại
+        console.log(err);
+      },
+      complete: () =>{
+        // xử lý logic hoàn thành
+        console.log("Done");
+      }
+    })
+        
+  }
 }
