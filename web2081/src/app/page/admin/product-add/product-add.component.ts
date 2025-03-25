a43bb4827ec19ba6d75c38b8bec8ca472036e793
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-03-25 20:07:08 +0700

    validate

--- web2081/src/app/page/admin/product-add/product-add.component.ts
+++ web2081/src/app/page/admin/product-add/product-add.component.ts
@@ -3,10 +3,11 @@ import { ProductAdd } from '../../../interface/product';
 import { FormsModule } from '@angular/forms';
 import { ProductService } from '../../../service/product.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-add',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './product-add.component.html',
   styleUrl: './product-add.component.css'
 })
@@ -21,17 +22,19 @@ export class ProductAddComponent {
 
   constructor(private productService: ProductService, private router: Router){}
 
-  handleSubmit(){
-    // console.log(this.product);
-    this.productService.add(this.product).subscribe({
-      next: ()=>{
-        alert("Thêm thành công")
-        this.router.navigate(['admin/product'])
-      },
-      error: (err) =>{
-        alert(err.message)
-      }
-    })
+  handleSubmit(form: any){
+    // console.log(form.valid);
+    if(form.valid){
+      this.productService.add(this.product).subscribe({
+        next: ()=>{
+          alert("Thêm thành công")
+          this.router.navigate(['admin/product'])
+        },
+        error: (err) =>{
+          alert(err.message)
+        }
+      })
+    }
   }
 
 }
