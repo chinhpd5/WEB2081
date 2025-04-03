e2c378dcaea6721fa5c63fa7b94d141d98b604bb
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-04-03 19:49:15 +0700

    login

--- web2081/src/app/page/login/login.component.ts
+++ web2081/src/app/page/login/login.component.ts
@@ -1,11 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  authForm!: FormGroup
+  
+    constructor(
+      private formBuilder: FormBuilder,
+      private authService: AuthService,
+      private router: Router
+    ){
+      this.authForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.pattern(/^\S+@\S+\.\S+$/)]],
+        password: ['',[Validators.required, Validators.minLength(6)]]
+      })
+    }
+  
+    handleSubmit(){
+      // console.log(this.authForm.valid);
+      if(this.authForm.invalid){
+        this.authForm.markAllAsTouched();// đánh dấu chạm cho tất cả các input
+        return;
+      }
+      // console.log(this.authForm.value);
+      this.authService.login(this.authForm.value).subscribe({
+        next: (data: any) =>{
+          console.log(data);
+          localStorage.setItem('token', data.accessToken)
+          alert("Đăng nhập thành công")
+          this.router.navigate(['/'])
+        },
+        error: (err) =>{
+          alert(err.error)
+        }
+      })
+      
+     
+    }
 
 }
--- web2081/src/app/service/auth.service.ts
+++ web2081/src/app/service/auth.service.ts
@@ -14,4 +14,8 @@ export class AuthService {
     return this.http.post('http://localhost:3000/register',data);
   }
 
+  login(data: any){
+    return this.http.post(`http://localhost:3000/login`,data)
+  }
+
 }
